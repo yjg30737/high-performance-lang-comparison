0e423af98befa47f676934bbaa774d6b0dd20db9
DESKTOP-V89Q18O\82109 <yjg30737@gmail.com>
2022-10-06 14:36:44 +0900

    Add cargo.toml, update a.rs to test performance of rust with the others

--- a.rs
+++ a.rs
@@ -1,3 +1,24 @@
+use rand::prelude::*;
+use std::time::SystemTime;
+
+fn timeit<F: Fn() -> T, T>(f: F) -> T {
+    let start = SystemTime::now();
+    let result = f();
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+    println!("it took {} seconds", duration.as_secs_f64());
+    result
+}
+
+fn rng_gen_mul() {
+    let mut rng = thread_rng();
+    for i in 0..10000000 {
+        let n1 = rng.gen::<f64>();
+        let n2 = rng.gen::<f64>();
+        let res: f64 = n1*n2;
+    }
+}
+  
 fn main() {
-    println!("Hello World!");
+    let x = timeit(|| rng_gen_mul());
 }
