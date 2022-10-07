6b19c206862d747872cd66c2ebb0d55aba37c5ee
DESKTOP-V89Q18O\82109 <yjg30737@gmail.com>
2022-10-07 14:54:14 +0900

    Change the Python value(i screwed up)

--- a.bat
+++ a.bat
@@ -1,7 +1,7 @@
 @echo off
-python a.py
 Rscript a.R
 go run a.go
+python a.py
 cargo run --release
 REM cargo run --release
 julia a.jl
--- a.py
+++ a.py
@@ -2,12 +2,11 @@ import numpy as np
 
 import time
 
-
 start_time = time.time()
 
-first = np.random.rand(100000000)
+first = np.random.rand(10000000)
 
-second = np.random.rand(100000000)
+second = np.random.rand(10000000)
 
 rst = np.multiply(first, second)
 
